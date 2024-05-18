d3105e0c13ec5c19a0921f216273948a16604123
prinzprogrammer <princeprogrammer73@gmail.com>
2024-05-18 21:26:41 +0800

    added utils.js file

---
+++ scripts/utils.js
@@ -0,0 +1,36 @@
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 1000);
+}
+
+function isWhiteSpaceOnly(input_string) {
+  return input_string.trim() === "";
+}
+
+function escapeHTMLCharacters(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
+    .replace(/`/g, "&#96;")
+    .replace(/\\/g, "\\\\")
+    .replace(/\$/g, "\\$");
+}
+
+function unescapeHTMLCharacters(str) {
+  return str
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&#96;/g, "`")
+    .replace(/&amp;/g, "&")
+    .replace(/\\\\/g, "\\")
+    .replace(/\\\$/g, "$");
+}
+
+function checkForHTMLCharacters(str) {
+  const htmlPattern = /&(lt|gt|quot|#39|#96|amp);|\\\\|/;
+  return htmlPattern.test(str);
+}
